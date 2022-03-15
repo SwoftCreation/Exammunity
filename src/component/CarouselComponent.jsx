66fc1cd25f4f222be1e56e99e46ee0f32e7ed775
SuwonLim <limsoft0401@kakao.com>
2022-03-16 07:47:12 +0900

    carousel image upload

--- src/component/CarouselComponent.jsx
+++ src/component/CarouselComponent.jsx
@@ -18,18 +18,18 @@ export default function CarouselComponent() {
           {
             altText: "Slide 1",
             key: 1,
-            src: "https://picsum.photos/id/458/1200/500",
+            src: "img/carousel1.png",
           },
           {
             altText: "Slide 2",
             key: 2,
-            src: "https://picsum.photos/id/456/1200/500",
+            src: "img/carousel2.png",
           },
           {
             altText: "Slide 3",
 
             key: 3,
-            src: "https://picsum.photos/id/678/1200/500",
+            src: "img/carousel3.png",
           },
         ]}
       />
