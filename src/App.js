50b11ba0bf6c0d8d1e19eba75738e071da068b96
SuwonLim <limsoft0401@kakao.com>
2022-03-16 09:59:44 +0900

    get rid of React-reveal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Zoom } from "react-reveal";
 
 import CarouselComponent from "./component/CarouselComponent";
 import FrontArticleComponent from "./component/FrontArticleComponent";
@@ -10,15 +9,16 @@ import NavbarComponent from "./component/NavbarComponent";
 function App() {
   return (
     <div>
-      <NavbarComponent dataAos="flipLeft" />
-      <Zoom>
-        <CarouselComponent dataAos="flipLeft" />
-      </Zoom>
+      <NavbarComponent />
+
+      <CarouselComponent />
+
       <br />
       <hr />
       <br />
+      <FrontArticleComponent />
 
-      <FrontArticleComponent dataAos="flipLeft" />
+      <p>Markup that will be revealed on scroll</p>
     </div>
   );
 }
