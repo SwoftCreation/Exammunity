6e3bf04fa8123250b74d30d2e22674795bf855d4
imsiyong <ehd970706@gmail.com>
2022-05-24 03:13:04 +0900

    memo on readme about server start, index.js path modify

--- README.md
+++ README.md
@@ -21,3 +21,10 @@ AOS.api
 
 데이터베이스
 amazonAWS
+
+-server 업데이트후 실행하기
+
+1. react파일 ">>npm run build" 명령어를 통해서 빌드 파일 업데이트
+2. /src/backEnd/server 폴더로 이동하기.
+3. npm start 이용하여 서버 실행하기.
+4. localhost:3000 주소로 접속.
--- src/backEnd/server/index.js
+++ src/backEnd/server/index.js
@@ -41,7 +41,7 @@ app.listen(port, () => {
   console.log(`Express app listening at port: ${port}`);
 });
 
-app.use(express.static(path.join(__dirname, "../Exammunity/build")));
+app.use(express.static(path.join(__dirname, "../../../build")));
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../Exammunity/build/index.html"));
+  res.sendFile(path.join(__dirname, "../../../build/index.html"));
 });
